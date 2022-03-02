60cd9484d2ef5596e62420765f94a7c6d18d9c99
heesungjang <heesungj7@gmail.com>
2022-03-02 17:10:02 +1000

    cout cin  endl

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 
-int main(void)
-{
-    int x = 2;
-    x = 5;
-    int y = x + 3;
+int main()
+{   
+    using namespace std;
 
-    std::cout << y;
+    int x = 3;
+    double  pi = 3.141592;
+
+    cout << "I love this lecture!" << endl;
+     
+    cout << "x is " << x << "pi is " << pi << endl;
+
+    int y;
+
+    cin >> y;
+
+    cout << "your input value is " << y << endl;
 
     return 0;
 }
