46a8c069a94cc7f1b88fabedcf97ab31860fb313
heesungjang <heesungj7@gmail.com>
2022-03-03 15:05:01 +1000

    cin, cin.get()

---
+++ demo-1.11.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int main()
+{
+    int num = 18;
+    int temp = 18 * 2;
+
+    std:: cout << "Num: " << num << ", Temp: " << temp << std::endl;
+    
+    return 0;
+}
---
+++ demo-1.15.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    char ch1, ch2, ch3;
+    cin.get(ch1);
+    cout << "char1 = " << ch1 << endl;
+    cin.get(ch2);
+    cout << "char2 = " << ch2 << endl;   
+    cin.get(ch3);
+    cout << "char3 = " << ch3 << endl;   
+
+
+    return 0;
+
+}
