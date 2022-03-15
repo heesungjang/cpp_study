76566b987ca5af3a6dc9a7e20593f398983a27ad
heesungjang <heesungj7@gmail.com>
2022-03-15 15:39:13 +1000

    constructor

---
+++ inflearn/constructors.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Fraction
+{
+private:
+    int m_numerator;
+    int m_denominator;
+
+public:
+    Fraction(const int& num_in, const int& den_in = 1)
+    {
+        m_numerator = num_in;
+        m_denominator = den_in;
+    }
+
+    void print()
+    {
+        cout << m_numerator << " / " << m_denominator << endl;
+    }
+};
+
+int main()
+{
+    Fraction one_thirds(1, 3);
+
+    one_thirds.print();
+
+    return 0;
+}
